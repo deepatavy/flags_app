59956e8a8d33bffb3db4df96953635f0b62261b9
Deep <deep.avy21@gmail.com>
2021-12-31 17:49:24 +0530

    Preference Added To check for data availability

--- lib/commons/constants.dart
+++ lib/commons/constants.dart
@@ -1,3 +1,4 @@
 class Constants {
  static String app_name = "Countries!";
+ static String key_data_loaded = "data_loaded";
 }
--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,6 +1,8 @@
+import 'package:flags_app/commons/constants.dart';
 import 'package:flags_app/screens/countries/country_list_screen.dart';
 import 'package:flags_app/screens/home/widgets/navigation_drawer_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -11,10 +13,18 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   List<String> regionList = ["Asia", "Africa", "Europe", "Middle East"];
+  late bool isDataLoaded;
+  late SharedPreferences prefs;
+
+  checkForDownload() async {
+    prefs = await SharedPreferences.getInstance();
+    isDataLoaded = prefs.getBool(Constants.key_data_loaded) ?? false;
+  }
 
   @override
   void initState() {
     super.initState();
+    checkForDownload();
   }
 
   @override
