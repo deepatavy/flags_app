4de01a9f3b3dff34d3d28394f8f79e0c38b6b3c6
Deep <deep.avy21@gmail.com>
2022-01-01 21:31:36 +0530

    Back Press Handled from home screen

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flags_app/api/services.dart';
 import 'package:flags_app/country_feed_bloc/bloc.dart';
+import 'package:flags_app/screens/home/widgets/exit_alert_widget.dart';
 import 'package:flags_app/screens/home/widgets/navigation_drawer_widget.dart';
 import 'package:flags_app/screens/home/widgets/region_list_widget.dart';
 import 'package:flutter/material.dart';
@@ -33,22 +34,25 @@ class _HomeScreenBuildState extends State<HomeScreenBuild> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Home"),
-        centerTitle: true,
+    return WillPopScope(
+      onWillPop: () => showExitAlertDialog(context),
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text("Home"),
+          centerTitle: true,
+        ),
+        drawer: NavigationDrawerWidget(),
+        body: BlocBuilder<CountryListBloc, CountryListState>(builder: (BuildContext context, CountryListState state) {
+          if (state is ErrorLoadingCountries) {
+            final error = state.error;
+            return Center(child: Text(error.message));
+          }
+          if (state is RegionListLoaded) {
+            return RegionListWidget(regionList: state.regionList);
+          }
+          return Center(child: CircularProgressIndicator());
+        }),
       ),
-      drawer: NavigationDrawerWidget(),
-      body: BlocBuilder<CountryListBloc, CountryListState>(builder: (BuildContext context, CountryListState state) {
-        if (state is ErrorLoadingCountries) {
-          final error = state.error;
-          return Center(child: Text(error.message));
-        }
-        if (state is RegionListLoaded) {
-          return RegionListWidget(regionList: state.regionList);
-        }
-        return Center(child: CircularProgressIndicator());
-      }),
     );
   }
 }
