c999e1814ac0bbdd2353c3f990711c8e6bc2a558
Deep <deep.avy21@gmail.com>
2022-01-01 21:56:30 +0530

    Cached Network Image Added For Image View

--- lib/screens/countries/widget/country_list_widget.dart
+++ lib/screens/countries/widget/country_list_widget.dart
@@ -1,9 +1,11 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flags_app/screens/home/model/country_model.dart';
 import 'package:flags_app/screens/neighbours/neighbour_country_list_screen.dart';
 import 'package:flutter/material.dart';
 
 class CountryListWidget extends StatelessWidget {
   final List<Country> countryList;
+
   const CountryListWidget({required this.countryList});
 
   @override
@@ -17,11 +19,33 @@ class CountryListWidget extends StatelessWidget {
             dense: false,
             tileColor: Colors.white,
             subtitle: Text(countryList[index].capital),
-            leading: Image.network(
-              countryList[index].flagUrl,
-              fit: BoxFit.cover,
+            leading: CachedNetworkImage(
+              imageUrl: countryList[index].flagUrl,
               height: 60,
               width: 60,
+              fit: BoxFit.cover,
+              errorWidget: (context, url, error) {
+                return Container(
+                  alignment: Alignment.center,
+                  height: 60,
+                  width: 60,
+                  color: Colors.red.withAlpha(80),
+                  child: Text(
+                    countryList[index].name[0],
+                    style: TextStyle(fontSize: 24),
+                  ),
+                );
+              },
+              placeholder: (_, __) => Container(
+                alignment: Alignment.center,
+                height: 60,
+                width: 60,
+                color: Colors.red.withAlpha(80),
+                child: Text(
+                  countryList[index].name[0],
+                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                ),
+              ),
             ),
             trailing: Icon(Icons.arrow_forward_ios),
             style: ListTileStyle.list,
@@ -31,7 +55,7 @@ class CountryListWidget extends StatelessWidget {
                 MaterialPageRoute(
                     builder: (context) => NeighbourCountryListScreen(
                           countryId: countryList[index].id,
-                      countryName: countryList[index].name,
+                          countryName: countryList[index].name,
                         )),
               );
             },
--- lib/screens/neighbours/widget/country_list_widget.dart
+++ lib/screens/neighbours/widget/country_list_widget.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flags_app/screens/home/model/country_model.dart';
 import 'package:flutter/material.dart';
 
@@ -18,11 +19,33 @@ class NeighbourCountryListWidget extends StatelessWidget {
             tileColor: Colors.white,
             subtitle:
                 Text(countryList[index].languages!.isNotEmpty ? countryList[index].languages!.values.toString() : ''),
-            leading: Image.network(
-              countryList[index].flagUrl,
-              fit: BoxFit.cover,
+            leading: CachedNetworkImage(
+              imageUrl: countryList[index].flagUrl,
               height: 60,
               width: 60,
+              fit: BoxFit.cover,
+              errorWidget: (context, url, error) {
+                return Container(
+                  alignment: Alignment.center,
+                  height: 60,
+                  width: 60,
+                  color: Colors.red.withAlpha(80),
+                  child: Text(
+                    countryList[index].name[0],
+                    style: TextStyle(fontSize: 24),
+                  ),
+                );
+              },
+              placeholder: (_, __) => Container(
+                alignment: Alignment.center,
+                height: 60,
+                width: 60,
+                color: Colors.red.withAlpha(80),
+                child: Text(
+                  countryList[index].name[0],
+                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                ),
+              ),
             ),
             style: ListTileStyle.list,
           ),
